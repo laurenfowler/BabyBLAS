fdb579b1f5adb2dd4ca7ee4805a3d7ce6d32ff0b
Lauren Fowler <lauren.michelle.fowler@live.mercer.edu>
2018-04-26 15:27:01 -0400

    lab machine pbs

---
+++ run_1.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+echo " 5 Threads "
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+./driver 9000 9000 10 5
+
+echo " 6 Threads "
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+./driver 9000 9000 10 6
+
+
+echo " 7 Threads "
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+./driver 9000 9000 10 7
+
+echo " 8 Threads "
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+./driver 9000 9000 10 8
+
+
+
+
+
+
+
